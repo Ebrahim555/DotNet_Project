b82bd51d95d2d54c681c56a06b197a25e31acfb4
Md Ebrahim Hossain <69958100+Ebrahim555@users.noreply.github.com>
2023-12-31 19:43:50 +0600

    First Commit

---
+++ PL/PL/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PL.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        [Route("api/empl/test")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                
+                return Request.CreateResponse(HttpStatusCode.OK,"Ebrahim");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
